e8726e50f2951c9c72d7649397f9f8ddb9b40a41
Matthew Smith <ms14981.2014@my.bristol.ac.uk>
2018-05-21 21:00:27 +0100

    Simulating a single play-through and logging progress.

--- src/bots/MonteCarloPlayer.js
+++ src/bots/MonteCarloPlayer.js
@@ -14,97 +14,31 @@ class MonteCarloPlayer {
     const captures = legalMoves.filter(move => /x/.test(move.san));
 
     const NUM_SIMULATIONS = 100;
-    console.log(chess)
-    // TODO: Write function to deep copy 'chess', equivalent to deep copying history
-    console.log(chess.history())
-    return chess.pickRandomMove(legalMoves);
+    const baselineHistory = chess.history().slice();
 
-    let AIPlayerColour = chess.turn();
-    let bestScore = 0;
-    let bestMove = legalMoves[0];
-    legalMoves.forEach(move => {
-      console.log("Looking at:")
-      console.log(move.san)
-      let newMoves = moves.concat(move);
-      chess.move(move, { sloppy: true })
-      if (chess.inCheckmate()) {
-        return chess.uci(move);
-      }
-      const opponentLegalMoves = chess.legalMoves();
-      let worstScore = 1;
-      let worstMove = opponentLegalMoves[0];
-      opponentLegalMoves.forEach(newMove => {
-        // chess.move(newMove, {sloppy:true});
-        let numWins = 0.0;
-        let depthLimit = 100;
-        for (let i = 0; i < NUM_SIMULATIONS; i++) {
+    let chosenMove = legalMoves[0];
 
-          let results = this.simulateUntilGameover(chess, newMove, false, depthLimit, 0);
-          // numWins += results[0];
-          // let numMovesToUndo = results[1];
-          // for (let j = 0; j < numMovesToUndo; j++) {
-          //   chess.undo();
-          // }
-          //console.log(numWins)
-        }
-        console.log(numWins);
+    console.log(baselineHistory);
+    console.log(legalMoves[0]);
 
-        let score = numWins / NUM_SIMULATIONS;
-        if (score < worstScore) {
-          worstScore = score;
-          worstMove = newMove;
-        }
-        //chess.undo();
-        // chess.reset();
-        // chess.applyMoves(moves)
-      });
-      chess.reset();
-      chess.applyMoves(moves)
-      if (worstScore > bestScore) {
-        bestScore = worstScore;
-        bestMove = move;
-      }
-      console.log(worstScore)
-    });
-    return chess.uci(bestMove);
+    this.simulateUntilGameover(chess);
 
-    // if (forcing.length) {
-    //   return chess.pickRandomMove(forcing);
-    // }
-    //
-    // if (captures.length) {
-    //   return chess.pickRandomMove(captures);
-    // }
-
-    if (legalMoves.length) {
-      return chess.pickRandomMove(legalMoves);
-    }
+    return chess.uci(chosenMove);
   }
 
-  simulateUntilGameover(chess, move, isAiTurn, depthLimit, currentDepth) {
-    if (chess.inCheckmate()) {
-      // console.log("Simulating")
-      // console.log(move)
-    }
-    // if (chess.inGameOver()) {
-    //   console.log("Simulating")
-    //   console.log(move)
-    // }
+  simulateUntilGameover(chess) {
+    let history = chess.history();
 
-    chess.move(move, { sloppy: true });
-    currentDepth++;
-    if (chess.inCheckmate() && isAiTurn) {
-      return [1, currentDepth];
-    } else if (chess.inCheckmate()) {
-      return [0, currentDepth];
-    } else if (chess.inDraw() || depthLimit <= 0) {
-      return [0.5, currentDepth];
+    while (!chess.inCheckmate() && !chess.inDraw()) {
+      console.log(chess.legalMoves().length);
+      let moves = chess.legalMoves();
+      let randomMove = moves[Math.floor(Math.random() * moves.length)];
+      chess.move(randomMove);
+    }
+    if (chess.inCheckmate()) {
+      return 1;
     } else {
-      chess.move(move, { sloppy: true });
-      const legalMoves = chess.legalMoves();
-      let randomMove = legalMoves[Math.floor(Math.random()*legalMoves.length)];
-      depthLimit--;
-      return [this.simulateUntilGameover(chess, randomMove, !isAiTurn, depthLimit), currentDepth];
+      return 0.5;
     }
   }
 
